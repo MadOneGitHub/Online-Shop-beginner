018482ca0687a86e8fcabe186c928d0ff398b708
MadOneGitHub <96055243+MadOneGitHub@users.noreply.github.com>
2023-06-15 20:48:59 +0200

    Add files via upload

---
+++ src/src/products.js
@@ -0,0 +1,72 @@
+import product1 from "./assets/products/1.jpg";
+import product2 from "./assets/products/2.jpg";
+import product3 from "./assets/products/3.jpg";
+import product4 from "./assets/products/4.jpg";
+import product5 from "./assets/products/5.jpg";
+import product6 from "./assets/products/6.jpg";
+import product7 from "./assets/products/7.jpg";
+import product8 from "./assets/products/8.jpg";
+import product9 from "./assets/products/9.jpg";
+import product10 from "./assets/products/10.jpg";
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Sohaan",
+    price: 5.99,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Gaz",
+    price: 4.99,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Safran",
+    price: 8.99,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Tablo-Farsch",
+    price: 99.99,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Mina",
+    price: 84.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Hasir",
+    price: 23.59,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Leather Bag",
+    price: 386.33,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Gelim",
+    price: 59.99,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Terme",
+    price: 112.0,
+    productImage: product9,
+  },
+  {
+    id: 10,
+    productName: "Mina-Exotisch",
+    price: 78.36,
+    productImage: product10,
+  },
+];
